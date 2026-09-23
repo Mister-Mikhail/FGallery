7673d4e1cee10ec6ec2ea1b3b5d5f771c763aacf
Mister-Mikhail <2335222@gmail.com>
2026-09-23 16:32:29 +0300

    Enable animated GIF and cached video thumbnails

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.7.6")
 
     implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    implementation("io.coil-kt.coil3:coil-gif:3.0.4")
+    implementation("io.coil-kt.coil3:coil-video:3.0.4")
     implementation("androidx.exifinterface:exifinterface:1.3.7")
     implementation("androidx.media3:media3-exoplayer:1.9.3")
     implementation("androidx.media3:media3-ui:1.9.3")
