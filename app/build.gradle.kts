6f875931e093556225c5e680766681e2c0fa919e
Mister-Mikhail <2335222@gmail.com>
2026-09-23 03:38:03 +0300

    Use stable Compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,19 +27,18 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2025.01.01"))
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
 
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.foundation:foundation")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
+    implementation("androidx.compose.foundation:foundation:1.7.6")
+    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("androidx.compose.material:material-icons-extended:1.7.6")
 
     implementation("io.coil-kt.coil3:coil-compose:3.0.4")
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.6")
 }
